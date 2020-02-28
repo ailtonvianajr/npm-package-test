246b8a327a6ea2741c467d37a2e899988911440c
Ailton Viana <ailtonvianajr@gmail.com>
2020-02-28 16:06:05 -0300

    first commit

---
+++ example/example.ts
@@ -0,0 +1,6 @@
+import * as calculator from '../src';
+
+console.log('sum', calculator.sum(9,5));
+console.log('multiply', calculator.multiply(9,5));
+console.log('divide', calculator.divide(9,5));
+console.log('subtract', calculator.subtract(9,5));
